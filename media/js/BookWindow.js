3e6204f3539f26c26f6d96a858253072c3b8c3ca
DESKTOP-RD8BMT6\apcom <apcom@live.ru>
2017-12-02 04:35:23 +0300

    Add additional files

---
+++ media/js/BookWindow.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class BookWindow {
+    constructor(book_id = 1) {
+        let target = this;
+
+        target.window = document.getElementsByClassName('book-window')[0];
+    }
+
+    show() {
+        let target = this;
+        this.window.classList.add('book-window--show');
+        this.overlay = new Overlay({
+            onDestroy: () => target.hide(),
+        });
+    }
+
+    hide() {
+        let target = this;
+        target.window.classList.remove('book-window--show');
+    }
+}
